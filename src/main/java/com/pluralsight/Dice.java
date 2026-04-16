c4d265298915dc3f8f1812cf6fd45c6bf535091b
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-16 14:20:04 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Dice {
+    public static void main(String[] args) {
+        public class Dice{
+            int roll(){
+                int randomNumber = (int)(Math.random( )* 6) +1;
+                return randomNumber;
+            }
+        }
+    }
+}
