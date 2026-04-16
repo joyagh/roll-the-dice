103ed08d7629717d1556084b430b9740aa11d280
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-16 14:26:08 -0400

    fixed file structure

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,12 +1,9 @@
 package com.pluralsight;
 
 public class Dice {
-    public static void main(String[] args) {
-        public class Dice{
-            int roll(){
+            int roll() {
                 int randomNumber = (int)(Math.random( )* 6) +1;
                 return randomNumber;
             }
         }
-    }
-}
+
