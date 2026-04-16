652249058280daac7c1d9080bd0d669744d16673
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-16 14:34:55 -0400

    added sice object and roll variables

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,5 +3,15 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
+        Dice dice = new Dice();
+
+        int roll1;
+        int roll2;
+
+        int twoCounter = 0;
+        int fourCounter = 0;
+        int sixCounter = 0;
+        int sevenCounter = 0;
+
     }
 }
